36d6e984e0db475d6993b57e4dac47438b733f5b
Samreen Islam <samreen.sfi@gmail.com>
2018-06-14 04:03:26 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
-
+function getIt() {
+    alert("Hey!");
+    return;
+}
+function frameIt() {
+    $('img').addClass("tasty");
+    return;
+}
+function pressIt() {
+  $("form").on("keydown", function(key) {
+    if(key.which == 71){
+      alert("You pressed G!");
+    }
+    return;
+  });
+}
+function submitIt() {
+    alert("Your form is going to be submitted now.");
+    return;
+}
 $(document).ready(function(){
 
 // call functions here
-
+$('p').on("click", getIt);
+$('img').on('load', frameIt);
+pressIt();
+$("form").on("submit", submitIt);
 });
