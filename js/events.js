e4f5d9e87ec303f909688c715097b81b492db86f
Samreen Islam <samreen.sfi@gmail.com>
2018-06-14 03:25:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,9 @@ function frameIt() {
 }
 function pressIt() {
   $("form").on("keydown", function(key) {
-    if(key.which == )
+    if(key.which == 71){
+      alert("You pressed G!")
+    }
   });
 }
 $(document).ready(function(){
